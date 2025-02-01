20981e71b77f7711534638cb944e417e38e28fc5
Egor Arsenyev <egrik93@gmail.com>
2025-02-01 16:00:34 +0300

    Complete tasks 1, 2, 3 of the homework assignment for lecture 3

--- Snake_game/src/main.c
+++ Snake_game/src/main.c
@@ -12,15 +12,24 @@ enum {MAX_TAIL_SIZE=100, START_TAIL_SIZE=3, MAX_FOOD_SIZE=20, FOOD_EXPIRE_SECOND
 
 
 // Здесь храним коды управления змейкой
-struct control_buttons
+typedef struct
 {
     int down;
     int up;
     int left;
     int right;
-}control_buttons;
+}control_buttons_t;
 
-struct control_buttons default_controls = {KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT};
+control_buttons_t default_controls = {KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT};
+
+/*
+ Хвост это массив состоящий из координат x,y
+ */
+typedef struct
+{
+    int x;
+    int y;
+} tail_t;
 
 /*
  Голова змейки содержит в себе
@@ -29,34 +38,25 @@ struct control_buttons default_controls = {KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT
  tsize - размер хвоста
  *tail -  ссылка на хвост
  */
-typedef struct snake_t
+typedef struct
 {
     int x;
     int y;
     int direction;
     size_t tsize;
-    struct tail_t *tail;
-    struct control_buttons controls;
-} snake_t;
+    tail_t *tail;
+    control_buttons_t controls;
+}snake_t;
 
-/*
- Хвост это массив состоящий из координат x,y
- */
-typedef struct tail_t
+void initTail(tail_t t[], size_t size)
 {
-    int x;
-    int y;
-} tail_t;
-
-void initTail(struct tail_t t[], size_t size)
-{
-    struct tail_t init_t={0,0};
+    tail_t init_t={0,0};
     for(size_t i=0; i<size; i++)
     {
         t[i]=init_t;
     }
 }
-void initHead(struct snake_t *head, int x, int y)
+void initHead(snake_t *head, int x, int y)
 {
     head->x = x;
     head->y = y;
@@ -74,38 +74,61 @@ tail_t*  tail  = (tail_t*) malloc(MAX_TAIL_SIZE*sizeof(tail_t));
 }
 
 /*
+Проверка на совпадение координат головы с любым сегментом хвоста
+*/
+int checkCollision(const snake_t *snake)
+{
+    for (size_t i = 0; i < snake->tsize; i++)
+    {
+        if (snake->x == snake->tail[i].x && snake->y == snake->tail[i].y)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
  Движение головы с учетом текущего направления движения
+ При переходе через границу терминала, змейка появляется на противоположной стороне
  */
-void go(struct snake_t *head)
+void go(snake_t *head)
 {
     char ch = '@';
-    int max_x=0, max_y=0;
-    getmaxyx(stdscr, max_y, max_x); // macro - размер терминала
-    mvprintw(head->y, head->x, " "); // очищаем один символ
+    int max_x = 0, max_y = 0;
+    getmaxyx(stdscr, max_y, max_x);
+    mvprintw(head->y, head->x, " ");
+
     switch (head->direction)
     {
         case LEFT:
-            if(head->x <= 0) // Циклическое движение, чтобы не
-// уходить за пределы экрана
-                head->x = max_x;
-            mvprintw(head->y, --(head->x), "%c", ch);
-        break;
+            head->x--;
+            if (head->x < 0)
+                head->x = max_x - 1;
+            break;
         case RIGHT:
-            mvprintw(head->y, ++(head->x), "%c", ch);
-        break;
+            head->x++;
+            if (head->x >= max_x)
+                head->x = 0;
+            break;
         case UP:
-            mvprintw(--(head->y), head->x, "%c", ch);
-        break;
+            head->y--;
+            if (head->y < MIN_Y)
+                head->y = max_y - 1;
+            break;
         case DOWN:
-            mvprintw(++(head->y), head->x, "%c", ch);
-        break;
+            head->y++;
+            if (head->y >= max_y)
+                head->y = MIN_Y;
+            break;
         default:
-        break;
+            break;
     }
+    mvprintw(head->y, head->x, "%c", ch);
     refresh();
 }
 
-void changeDirection(struct snake_t* snake, const int32_t key)
+void changeDirection(snake_t* snake, const int32_t key)
 {
     if (key == snake->controls.down)
         snake->direction = DOWN;
@@ -120,7 +143,7 @@ void changeDirection(struct snake_t* snake, const int32_t key)
 /*
  Движение хвоста с учетом движения головы
  */
-void goTail(struct snake_t *head)
+void goTail(snake_t *head)
 {
     char ch = '*';
     mvprintw(head->tail[head->tsize-1].y, head->tail[head->tsize-1].x, " ");
@@ -136,26 +159,49 @@ void goTail(struct snake_t *head)
 
 int main()
 {
-snake_t* snake = (snake_t*)malloc(sizeof(snake_t));
-    initSnake(snake,START_TAIL_SIZE,10,10);
+    snake_t* snake = (snake_t*)malloc(sizeof(snake_t));
+    initSnake(snake, START_TAIL_SIZE, 10, 10);
     initscr();
-    keypad(stdscr, TRUE); // Включаем F1, F2, стрелки и т.д.
-    raw();                // Откдючаем line buffering
-    noecho();            // Отключаем echo() режим при вызове getch
-    curs_set(FALSE);    //Отключаем курсор
-    mvprintw(0, 0,"Use arrows for control. Press 'F10' for EXIT");
-    timeout(0);    //Отключаем таймаут после нажатия клавиши в цикле
-    int key_pressed=0;
-    while( key_pressed != STOP_GAME )
+    keypad(stdscr, TRUE);
+    raw();
+    noecho();
+    curs_set(FALSE);
+    mvprintw(1, 0, "Use arrows for control. Press 'F10' for EXIT");
+    timeout(0);
+
+    clock_t last_time = clock();
+    const double frame_delay = 100; // 100 ms per frame
+    int key_pressed = 0;
+    int game_over = 0;
+
+    while (key_pressed != STOP_GAME && !game_over)
     {
-        key_pressed = getch(); // Считываем клавишу
-        go(snake);
-        goTail(snake);
-        timeout(100); // Задержка при отрисовке
-        changeDirection(snake, key_pressed);
+        clock_t current_time = clock();
+        double elapsed = (double)(current_time - last_time) * 50000 / CLOCKS_PER_SEC;
+
+        if (elapsed >= frame_delay)
+        {
+            last_time = current_time;
+            key_pressed = getch();
+            go(snake);
+            if (checkCollision(snake))
+            {
+                int max_y, max_x;
+                getmaxyx(stdscr, max_y, max_x);
+                mvprintw(max_y / 2, max_x / 2 - 5, "GAME OVER!");
+                refresh();
+                sleep(2);
+                game_over = 1;
+                break;
+            }
+            goTail(snake);
+            changeDirection(snake, key_pressed);
+        }
+        usleep(1000); // Reduce CPU usage
     }
+
     free(snake->tail);
     free(snake);
-    endwin(); // Завершаем режим curses mod
+    endwin();
     return 0;
 }
